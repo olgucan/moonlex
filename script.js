3a2e7cd587cb4865f4a51fff61f7a58ca8135052
olgucan <olgucan@4alabs.io>
2023-07-03 12:50:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+//let nav=document.querySelector("nav")
+//console.log(nav)
+window.addEventListener("scroll",scrollFunc)
+let newnav=document.querySelector("nav")
+let item =document.querySelector(".center-heading")
+function scrollFunc() {
+    const windowheight=window.innerHeight
+    let navheight=item.getBoundingClientRect().top
+    
+    //console.log(navheight)
+    if(navheight!==0){
+        newnav.classList.add("newnav")
+    }
+    
+    else {
+        newnav.classList.remove("newnav")
+    }
+}
+// let ul= document.querySelector("nav")
+// document.querySelector(".fa-solid.fa-bars").addEventListener("click",(e)=>{
+   
+//         ul.classList.toggle("toggle")
+// })
+function Domload() {
+    let container =document.createElement("div")
+    let div=document.createElement("div")
+    container.classList.add("lastcon")
+    container.style.position="fixed"
+    container.style.top="70px"
+    container.style.width="100%"
+    container.style.zIndex="999"
+    container.style.background="transparent"
+    div.style.width="90%"
+    div.style.margin="0 auto"
+    div.style.display="flex"
+    div.style.flexDirection="column"
+    div.style.alignItems="flex-start"
+    div.style.backgroundColor="white"
+    container.appendChild(div)
+//let section1 =document.querySelector(".section1")
+document.querySelector(".hamburgercontan").addEventListener("click",(e)=>{
+    //console.log("adsad")
+    e.currentTarget.firstElementChild.classList.toggle("toggle")
+    e.currentTarget.classList.toggle("toggle")
+    if (e.currentTarget.classList.contains("toggle")){
+        div.innerHTML=`
+        <a href='about.html'  style='width:100%;display:block;border-bottom:1px solid ;text-decoration:none;padding-top:10px;padding-left:10px;color:black;margin-bottom:10px'>About Us</a>
+        <a href='products.html' style='width:100%;display:block;border-bottom:1px solid ;text-decoration:none;padding-left:10px;padding-top:0px;color:black;margin-bottom:0px'>Products <i style='color:black' class='fa-solid fa-caret-down'></i></a>
+        <a href='contact.html' style='width:100%;display:block;border-bottom:1px solid ;text-decoration:none;color:black;margin-bottom:0px;padding-left:10px;padding-top:10px'>Contact Us</a>
+        <a href='#' style='width:100%;display:block;border-bottom:1px solid ;text-decoration:none;color:black;margin-bottom:0px;padding-left:10px;padding-top:10px;padding-bottom:10px'>Certification</a>
+        `
+        //div.classList.add("newmenu")
+    
+        document.body.appendChild(container)
+    }
+    else {
+         document.body.removeChild(container)
+    }
+})
+}
+
+document.addEventListener("DOMContentLoaded",Domload)
